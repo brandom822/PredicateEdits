24bdf2b7f946cfba2b08565f878e3a34247e772c
NangLe89 <nang.le1989@gmail.com>
2015-04-15 03:18:02 -0400

    name add
    a

--- sim-bpred.c
+++ sim-bpred.c
@@ -1,6 +1,6 @@
 /* sim-bpred.c - sample branch predictor simulator implementation */
 
-/* SimpleScalar(TM) Tool Suite ---- Nang Lam Le
+/* SimpleScalar(TM) Tool Suite ---- Nang Lam Le & Brandon McMillian
  * Copyright (C) 1994-2003 by Todd M. Austin, Ph.D. and SimpleScalar, LLC.
  * All Rights Reserved.
  *
